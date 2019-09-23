a100aaa9afd22caa9b1b24c617f2270f13cc0928
KHobden <40343874+KHobden@users.noreply.github.com>
2019-09-23 15:09:28 +0100

    Initial commit

---
+++ lenet.py
@@ -0,0 +1,50 @@
+#Convolutional Neural Network
+#Kieran Hobden
+#20-Sep-'19
+
+from keras.models import Sequential
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+from keras.layers.core import Flatten
+from keras.layers.core import Dense
+from keras import backend as K
+
+class LeNet:
+    @staticmethod
+    def build(numChannels, imgRows, imgCols, numClasses, activation="relu", weightsPath=None):
+        #Initialise the model
+        model = Sequential()
+        inputShape = (imgRows, imgCols, numChannels)
+
+        #Update the input shape if channels first is used
+        if K.image_data_format() == "channels_first":
+            inputShape = (numChannels, imgRows, imgCols)
+
+        #Define the first set of CONV => ACTIVATION => POOL layers
+        model.add(Conv2D(20, 5, padding="same", input_shape=inputShape))
+        model.add(Activation(activation))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+        #Define the second set of CONV => ACTIVATION => POOL layers
+        model.add(Conv2D(50, 5, padding="same"))
+        model.add(Activation(activation))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+        #Define the first FC => ACTIVATION layers
+        model.add(Flatten())
+        model.add(Dense(500))
+        model.add(Activation(activation))
+
+        #Define the second FC layer
+        model.add(Dense(numClasses))
+
+        #Lastly, define the soft-max classifier
+        model.add(Activation("softmax"))
+
+        #If the model was pre-trained and a weights path is supplied, load the weights
+        if weightsPath is not None:
+            model.load_weights(weightsPath)
+
+        #Return the constructed network architecture
+        return model
---
+++ lenet_mnist.py
@@ -0,0 +1,76 @@
+#LeNet Driver Script
+#Kieran Hobden
+#20-Sep-'19
+
+from lenet import LeNet
+from sklearn.model_selection import train_test_split
+from keras.datasets import mnist
+from keras.optimizers import SGD
+from keras.utils import np_utils
+from keras import backend as K
+import numpy as np
+import argparse
+
+#Construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-s", "--save-model", type=int, default=-1,
+	help="(optional) whether or not model should be saved to disk")
+ap.add_argument("-l", "--load-model", type=int, default=-1,
+	help="(optional) whether or not pre-trained model should be loaded")
+ap.add_argument("-w", "--weights", type=str,
+	help="(optional) path to weights file")
+args = vars(ap.parse_args())
+
+#Download the MNIST dataset
+print("[INFO] downloading MNIST...")
+((trainData, trainLabels), (testData, testLabels)) = mnist.load_data()
+
+#If "channels first" ordering, reshape the design matrix to num_samples x depth x rows x columns
+if K.image_data_format() == "channels_first":
+	trainData = trainData.reshape((trainData.shape[0], 1, 28, 28))
+	testData = testData.reshape((testData.shape[0], 1, 28, 28))
+#Otherwise "channels last" ordering, reshape the design matrix shape to num_samples x rows x columns x depth
+else:
+	trainData = trainData.reshape((trainData.shape[0], 28, 28, 1))
+	testData = testData.reshape((testData.shape[0], 28, 28, 1))
+
+#Scale data to the range of [0, 1]
+trainData = trainData.astype("float32") / 255.0
+testData = testData.astype("float32") / 255.0
+
+#Process labels for the categorical cross-entropy loss function
+trainLabels = np_utils.to_categorical(trainLabels, 10)
+testLabels = np_utils.to_categorical(testLabels, 10)
+
+#Initialize the optimizer and model
+print("[INFO] compiling model...")
+opt = SGD(lr=0.01)
+model = LeNet.build(numChannels=1, imgRows=28, imgCols=28,
+	numClasses=10,
+	weightsPath=args["weights"] if args["load_model"] > 0 else None)
+model.compile(loss="categorical_crossentropy", optimizer=opt,
+	metrics=["accuracy"])
+
+#Train and evaluate the model only if not loading a pre-existing model
+if args["load_model"] < 0:
+	print("[INFO] training...")
+	model.fit(trainData, trainLabels, batch_size=128, epochs=20,
+		verbose=1)
+
+	#Print the accuracy on the testing set
+	print("[INFO] evaluating...")
+	(loss, accuracy) = model.evaluate(testData, testLabels,
+		batch_size=128, verbose=1)
+	print("[INFO] accuracy: {:.2f}%".format(accuracy * 100))
+
+#Save model to .hdf5 file
+if args["save_model"] > 0:
+	print("[INFO] dumping weights to file...")
+	model.save_weights(args["weights"], overwrite=True)
+
+#Randomly select testing digits ro prove program success
+for i in np.random.choice(np.arange(0, len(testLabels)), size=(10,)):
+	probs = model.predict(testData[np.newaxis, i])
+	prediction = probs.argmax(axis=1)
+	print("[INFO] Predicted: {}, Actual: {}".format(prediction[0],
+		np.argmax(testLabels[i])))
